17c2f58fa4409cfe33f386a7f016e118b5529eb8
dasepmoch <108404480+dasepmoch@users.noreply.github.com>
2025-04-19 20:02:31 +0700

    bbabba

--- src/index.js
+++ src/index.js
@@ -8,20 +8,13 @@ export default {
     const wasabiRequest = new Request(wasabiUrl, {
       method: request.method,
       headers: request.headers,
-      redirect: 'manual',
+      redirect: 'manual'
     });
 
     const originResponse = await fetch(wasabiRequest);
+
     const headers = new Headers(originResponse.headers);
-
-    // Deteksi apakah ini permintaan streaming parsial (Range)
-    const isRangeRequest = request.headers.has("range");
-    const isPartial = originResponse.status === 206;
-
-    // Kalau bukan Range dan bukan 206, kita cache
-    if (!isRangeRequest && !isPartial) {
-      headers.set("Cache-Control", "public, max-age=31536000, immutable");
-    }
+    headers.set('Cache-Control', 'public, max-age=31536000, immutable');
 
     return new Response(originResponse.body, {
       status: originResponse.status,
