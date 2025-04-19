cb835d501eca5632134435d3d9b9639fea94ff74
dasepmoch <108404480+dasepmoch@users.noreply.github.com>
2025-04-19 18:47:20 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,16 @@ export default {
   async fetch(request) {
     const url = new URL(request.url);
     const path = url.pathname;
+
     const wasabiUrl = `https://s3.ap-southeast-1.wasabisys.com/trenpedia${path}`;
-    return fetch(wasabiUrl);
+    const originResponse = await fetch(wasabiUrl);
+
+    const newHeaders = new Headers(originResponse.headers);
+    newHeaders.set('Cache-Control', 'public, max-age=31536000, immutable');
+
+    return new Response(originResponse.body, {
+      status: originResponse.status,
+      headers: newHeaders
+    });
   }
 }
