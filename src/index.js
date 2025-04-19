a7d6e2d6df7aed16709d872a2e090af773dcc35e
dasepmoch <108404480+dasepmoch@users.noreply.github.com>
2025-04-19 19:13:14 +0700

    Terbaru

--- src/index.js
+++ src/index.js
@@ -4,14 +4,25 @@ export default {
     const path = url.pathname;
 
     const wasabiUrl = `https://s3.ap-southeast-1.wasabisys.com/trenpedia${path}`;
-    const originResponse = await fetch(wasabiUrl);
 
-    const newHeaders = new Headers(originResponse.headers);
-    newHeaders.set('Cache-Control', 'public, max-age=31536000, immutable');
+    // Teruskan semua header asli dari user ke Wasabi (termasuk Range!)
+    const wasabiRequest = new Request(wasabiUrl, {
+      method: request.method,
+      headers: request.headers,
+    });
+
+    const originResponse = await fetch(wasabiRequest);
+
+    // Copy semua header dari Wasabi
+    const headers = new Headers(originResponse.headers);
+
+    // Tambahkan cache-control (ini tidak overwrite Range)
+    headers.set('Cache-Control', 'public, max-age=31536000, immutable');
 
     return new Response(originResponse.body, {
       status: originResponse.status,
-      headers: newHeaders
+      statusText: originResponse.statusText,
+      headers,
     });
   }
 }
