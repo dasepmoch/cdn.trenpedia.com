711675b79030fa463fe57f11c8005a8f7aa66ac8
dasepmoch <108404480+dasepmoch@users.noreply.github.com>
2025-04-19 19:45:17 +0700

    baa

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export default {
 
     const wasabiUrl = `https://s3.ap-southeast-1.wasabisys.com/trenpedia${path}`;
 
+    // Kirim request ke Wasabi dengan semua header, termasuk Range
     const wasabiRequest = new Request(wasabiUrl, {
       method: request.method,
       headers: request.headers,
@@ -13,6 +14,25 @@ export default {
 
     const originResponse = await fetch(wasabiRequest);
 
+    // Kalau Wasabi kasih redirect (jarang tapi bisa)
+    if (originResponse.status === 302) {
+      const redirectUrl = originResponse.headers.get('Location');
+      const finalResponse = await fetch(redirectUrl, {
+        method: request.method,
+        headers: request.headers
+      });
+
+      const finalHeaders = new Headers(finalResponse.headers);
+      finalHeaders.set('Cache-Control', 'public, max-age=31536000, immutable');
+
+      return new Response(finalResponse.body, {
+        status: finalResponse.status,
+        statusText: finalResponse.statusText,
+        headers: finalHeaders
+      });
+    }
+
+    // Kalau tidak redirect, langsung kirim balik respon Wasabi
     const headers = new Headers(originResponse.headers);
     headers.set('Cache-Control', 'public, max-age=31536000, immutable');
 
